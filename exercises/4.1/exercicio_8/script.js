a1b2806f6eebde0a3cf15b3eb3bbadba99806fd0
¨ViniciusCarvalho-Tribo13-B¨ <vinicius.10.carvalho.100@gmail.com>
2021-05-19 20:09:33 -0300

    b.4 d.1 exercicio 8

---
+++ exercises/4.1/exercicio_8/script.js
@@ -0,0 +1,12 @@
+let numero1 = 10;
+let numero2 = 9;
+let numero3 = 7;
+let par1 = (numero1 % 2) == 0;
+let par2 = (numero2 % 2) == 0;
+let par3 = (numero3 % 2) == 0;
+
+if (par1 == true || par2 == true || par3 == true) {
+  console.log(true)
+} else {
+  console.log(false)
+}
