ad5ad0b2ca16c9258458e57f844d5b5dcaaedd9f
¨ViniciusCarvalho-Tribo13-B¨ <vinicius.10.carvalho.100@gmail.com>
2021-05-19 16:12:50 -0300

    b.4 d.1 exercicio 2

---
+++ exercises/4.1/exercicio_2/scripts.js
@@ -0,0 +1,10 @@
+let a = 5;
+let b = 5;
+
+if (a > b) {
+  console.log("o maior é o valor de a que é " + a)
+} else if (b > a) {
+  console.log("o maior é o valor de b que é " + b)
+} else {
+  console.log("o valor de a e b são iguais")
+}
