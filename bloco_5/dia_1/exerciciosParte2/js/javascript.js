01e426716d67a48b68ca0a83ea1c9e03d713734e
¨ViniciusCarvalho-Tribo13-B¨ <vinicius.10.carvalho.100@gmail.com>
2021-05-26 16:00:59 -0300

    b5-d1 exercicios 1, 2, 3 ID

---
+++ bloco_5/dia_1/exerciciosParte2/js/javascript.js
@@ -0,0 +1,6 @@
+// exercicio 1 ID
+document.getElementById('page-title').innerText = 'Trilogia Bourne';
+// exercicio 2 ID
+document.getElementById('second-paragraph').innerText = 'Eu sei que o Trilogia Bourne são três filmes!';
+// exercicio 3 ID
+document.getElementById('subtitle').innerText = 'Explicação sobre o titulo!'
