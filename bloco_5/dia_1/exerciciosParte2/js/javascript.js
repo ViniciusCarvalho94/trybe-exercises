9364bb4cc802640d575109be8c108d7d21524997
¨ViniciusCarvalho-Tribo13-B¨ <vinicius.10.carvalho.100@gmail.com>
2021-05-26 18:06:00 -0300

    b5-d1 exercicios 1, 2, 3, 4 CLASS e TAG

--- bloco_5/dia_1/exerciciosParte2/js/javascript.js
+++ bloco_5/dia_1/exerciciosParte2/js/javascript.js
@@ -1,6 +1,21 @@
 // exercicio 1 ID
 document.getElementById('page-title').innerText = 'Trilogia Bourne';
+
 // exercicio 2 ID
 document.getElementById('second-paragraph').innerText = 'Eu sei que o Trilogia Bourne são três filmes!';
+
 // exercicio 3 ID
 document.getElementById('subtitle').innerText = 'Explicação sobre o titulo!'
+
+// ===========================
+
+// exercicio 2 CLASS
+document.getElementsByClassName('paragrafos')
+
+// exercicio 3 CLASS
+const paragrafo = document.getElementsByClassName('paragrafos')[0]
+paragrafo.style.background = 'lightgray';
+
+// exercicio 4 TAG
+const paragrafo2 = document.getElementsByTagName('h4')[0];
+paragrafo2.style.color = 'blue';
