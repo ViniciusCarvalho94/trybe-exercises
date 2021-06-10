d7e98b69db3febfe9c7291dec1ee83268dd6f72c
¨ViniciusCarvalho-Tribo13-B¨ <vinicius.10.carvalho.100@gmail.com>
2021-06-10 16:19:43 -0300

    b6d2 exercicio 1

---
+++ exercises/6.1-6.2/js/script.js
@@ -0,0 +1,52 @@
+function newState(value, name) {
+  const dropdownState = document.getElementById('dropdown-state');
+  let state = document.createElement('option');
+  state.value = value;
+  state.text = name;
+  dropdownState.appendChild(state);
+}
+
+newState('', 'Selecione')
+newState('ac', 'Acre')
+newState('al', 'Alagoas')
+newState('ap', 'Amapá')
+newState('ba', 'Bahia')
+newState('df', 'Distrito Federal')
+newState('es', 'Espírito Santo')
+newState('go', 'Goiás')
+newState('ma', 'Maranhão')
+newState('mt', 'Mato Grosso')
+newState('ms', 'Mato Grosso do Sul')
+newState('mg', 'Minas Gerais')
+newState('pa', 'Pará')
+newState('pb', 'Paraíba')
+newState('pr', 'Paraná')
+newState('pe', 'Pernambuco')
+newState('pi', 'Piauí')
+newState('rj', 'Rio de Janeiro')
+newState('rn', 'Rio Grande do Norte')
+newState('rs', 'Rio Grande do Sul')
+newState('ro', 'Rondônia')
+newState('rr', 'Roraima')
+newState('sc', 'Santa Catarina')
+newState('sp', 'São Paulo')
+newState('se', 'Sergipe')
+newState('to', 'Tocantins')
+
+function formatCPF(cpf) {
+  const elementoAlvo = cpf
+  const cpfAtual = cpf.value   
+  let cpfAtualizado;
+  cpfAtualizado = cpfAtual.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, 
+    function( regex, argumento1, argumento2, argumento3, argumento4 ) {
+      return argumento1 + '.' + argumento2 + '.' + argumento3 + '-' + argumento4;
+  })  
+  elementoAlvo.value = cpfAtualizado; 
+}
+
+const form = document.querySelector('form');
+const sumit = document.getElementById('submit');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+})
