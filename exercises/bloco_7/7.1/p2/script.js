fd6e7733f74cd43742eb19b1151aea66bf048a7a
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-17 19:46:15 -0300

    b7d1 p2 e2 fix, e3

--- exercises/bloco_7/7.1/p2/script.js
+++ exercises/bloco_7/7.1/p2/script.js
@@ -1,18 +1,19 @@
 //exercicio 1
 
-const factorial = (number) => number === 0 ? 1 : number * factorial(number - 1)
+const factorial = (number) =>
+  number === 0 ? 1 : number * factorial(number - 1);
 
-console.log(factorial(4))
-
+console.log(factorial(4));
 
 //exercicio 2
 
-const longestWord = 'Antônio foi no banheiro e não sabemos o que aconteceu';
-let bigWord = '';
-longestWord.split(' ').forEach(word => {
-  if (word.trim().length > bigWord.length) {
-    bigWord = word.trim();
-  }
-})
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0]
 
-console.log(bigWord)
+console.log(longestWord("Antonio foi no banheiro e não sabemos o que aconteceu"))
+
+//exercicio 3
+
+let clickCount = 0
+let textToDisplay = document.getElementById("text")
+
+document.getElementById("button_test").addEventListener("click", () => textToDisplay.innerHTML = clickCount += 1)
