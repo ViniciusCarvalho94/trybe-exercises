fbedaaf984c186eb606dcabcb73d277c5c7505da
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-21 20:13:54 -0300

    b7d3 p3 e1

---
+++ exercises/bloco_7/7.3/p3/e1/script.js
@@ -0,0 +1,17 @@
+const assert = require('assert')
+
+const greetPeople = (parameter) => {
+  let greeting = 'Hello ';
+  let persons = []
+  for (const i in parameter) {
+    persons.push(greeting + parameter[i])
+  }
+  return persons;
+};
+
+const parameter = ['Irina', 'Ashleigh', 'Elsa'];
+const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa']
+
+assert.strictEqual(typeof(greetPeople), 'function')
+
+assert.deepStrictEqual(greetPeople(parameter), result)
