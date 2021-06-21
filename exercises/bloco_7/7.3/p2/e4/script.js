eea0029a5fbe9be683d2dff16e5fb0397f35957e
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-21 20:00:09 -0300

    b7d3 p2 e4

---
+++ exercises/bloco_7/7.3/p2/e4/script.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+
+const findTheNeedle = (words, word) => {
+  let output = -1
+  for (i = 0; i <= words.length; i += 1) {
+    if (words[i] === word) {
+      return i
+    } 
+  }
+  return output
+}
+
+let words = ['house', 'train', 'slide', 'needle', 'book'];
+let expected = 3;
+let output = findTheNeedle(words, 'needle');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = 0;
+output = findTheNeedle(words, 'plant');
+assert.strictEqual(output, expected);
+
+words = ['plant', 'shelf', 'arrow', 'bird'];
+expected = -1;
+output = findTheNeedle(words, 'plat');
+assert.strictEqual(output, expected);
