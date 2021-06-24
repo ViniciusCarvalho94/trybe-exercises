319e3bdca55b737ae87371cb01b53539f335bc42
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-24 14:08:54 -0300

    b8e1 e3 fix indentation

--- exercises/bloco_8/exercicios/e3/script.js
+++ exercises/bloco_8/exercicios/e3/script.js
@@ -5,18 +5,18 @@ const correct = (answers, student) => {
   let grades = 0;
   for (i = 0; i < 10; i += 1) {
     if (answers[i] === student[i]) {
-      grades += 1
+      grades += 1;
     } else if (student[i] === 'N.A') {
-      grades = grades
+      grades = grades;
     } else {
-     grades -= 0.5
+      grades -= 0.5;
     }
   }
-  return grades
-}
+  return grades;
+};
 
 const right = (answers, student, callback) => {
-  return callback(answers, student)
-}
+  return callback(answers, student);
+};
 
-console.log(right(rightAnswers, studentAnswers, correct))
+console.log(right(rightAnswers, studentAnswers, correct));
