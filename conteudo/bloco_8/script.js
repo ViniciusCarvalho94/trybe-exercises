3c4b21350635e481b7d316ca06cd0280e7c915ed
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-24 15:42:53 -0300

    arrumando pasta

--- conteudo/bloco_8/script.js
+++
@@ -1,9 +0,0 @@
-const wakeUp = () => console.log('Acordando!!');
-
-const coffe = () => console.log('Bora tomar café!!');
-
-const sleep = () => console.log('Partiu dormir!!');
-
-const doingThings = (param) => param();
-
-doingThings(wakeUp)
