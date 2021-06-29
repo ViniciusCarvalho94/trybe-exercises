f7fbf7ab0af599d84702c12c3ff6e88436081b08
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-06-29 15:21:09 -0300

    b8d5 conteudo

---
+++ conteudo/bloco_8/dia_5/c4/e2/script.js
@@ -0,0 +1,9 @@
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+const [rice, cat, water] = [bebida, comida, animal]
+console.log(rice, cat, water)
