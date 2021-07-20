ae698f36fd365914e9ee006f2159222fd251415c
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-07-20 15:42:50 -0300

    b11d1 conteudo

---
+++ conteudo/modulo-2-Desenvolvimento-Front-end/bloco_11-Introducao-a-React/dia_1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Description from './Component';
+import './App.css';
+
+function App() {
+  return (
+    <Description />
+  );
+}
+
+export default App;
---
+++ conteudo/modulo-2-Desenvolvimento-Front-end/bloco_11-Introducao-a-React/dia_1/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class description extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Vinicius Albuquerque de Carvalho</h1>
+        <p>Sou um rapaz Paulistano de 27 anos SP forever :D</p>
+      </div>
+    );
+  };
+};
+
+export default description;
