d8fc54909955e6888485fdd5d2756bf6d9c61c2b
Vinicius <vinicius.10.carvalho.100@gmail.com>
2021-07-30 18:37:45 -0300

    b13d2 conteudo

---
+++ conteudo/modulo-2-Desenvolvimento-Front-end/bloco_13-Ciclo-de-Vida-de-Componentes-e-React-Router/dia_2/src/components/Users.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    const { greetingsMessage } = this.props
+    const { id } = this.props.match.params
+    return (
+      <div>
+        <h2>Users</h2>
+        <p> { greetingsMessage }, My awesome Users component with id { id } </p>
+      </div>
+    );
+  }
+};
+
+export default Users;
+
+
